21339b07d417c093b7e04cc251f81ff8aae24f0a
sproels <sanne.roels@ugent.be>
2016-05-18 14:36:00 +0200

    executable file taking four arguments
    The file is now executable from baseline taking 4 arguments

--- change_header.py
+++ change_header.py
@@ -1,10 +1,28 @@
+#!/usr/bin/python
+import sys
 import numpy as np
 import os
 import nibabel as nib
-# set data_path -> fix it in a loop
-data_path="/Users/pp01sanne/Downloads/"
+
+#HOW TO USE this file
+# it takes 4 arguments sys.argv[0] is the command itself
+old_path=sys.argv[1]
+new_path=sys.argv[2]
+old_file=sys.argv[3]
+new_file=sys.argv[4]
+
+#"/Users/pp01sanne/Downloads/"
+#'preproc_2.nii'
+#
+#
+#"/Users/pp01sanne/Downloads/"
+print(old_path)
+print(new_path)
+print(old_file)
+print(new_file)
+
 # load the nifti file
-example_ni1 = os.path.join(data_path, 'preproc_2.nii')
+example_ni1 = os.path.join(old_path, old_file)
 n1_img = nib.load(example_ni1)
 # change the header [2]
 n1_img.header['pixdim']=np.array([ 1.,  1.,  1.,  1.,  2.,  0.,  0.,  0.])
@@ -14,5 +32,5 @@ affine=n1_img.get_affine()
 # make the new image 
 new_img = nib.Nifti1Image(data, affine, header=n1_img.header)
 # save the new image
-nib.save(new_img, os.path.join(data_path,'test4d.nii.gz'))
-print(new_img.header)
+nib.save(new_img, os.path.join(new_path, new_file))
+#print(new_img.header)
